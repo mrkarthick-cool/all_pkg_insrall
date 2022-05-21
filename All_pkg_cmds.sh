4fc2e718e2a9e7036c65e3dc0da36b67cf4b1966
Mrkarthick <78952385+mrkarthick-cool@users.noreply.github.com>
2022-05-21 15:03:22 +0530

    bash file
    This tool is basic tool for green hat hackers
    
    0 lvl knowledge  of hacking. Use  this tool frist 
    
    Next run any GitHub tool without any error

---
+++ All_pkg_cmds.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+clear
+
+echo ""------------------------------------------------------""
+# we are Anonymous
+
+ sleep 0.19
+echo ""
+echo ""
+
+ echo " If You Connected To FAST Internet Then  "
+sleep 0.19
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+sleep 0.19
+echo ""
+echo ""
+echo ""------------------------------------------------------""
+ sleep 1
+ sleep 0.19
+ echo  "#####################################"
+echo ""
+echo ""
+ echo  " FOLLOW ME  INSTAGRAM "
+echo ""
+
+ echo  "#####################################"
+
+ sleep o.19
+
+ sleep 1
+
+  clear
+
+echo  "We are Anonymous,"
+sleep 0.19
+echo "  We are legion, "
+sleep 0.19
+echo "  We do not forget,"
+sleep 0.19
+echo "  We do not forgive,"
+sleep 0.19
+echo "  Expect Us. "
+sleep 0.19
+
+echo $green
+echo ""------------------------------------------------------""
+echo ""
+
+echo " This Script is Made by Rootkarthick from chennai "
+echo ""
+echo ""------------------------------------------------------ ""
+echo
+
+sleep 3
+
+clear
+
+
+echo  "#####################################"echo ""
+
+echo "  I LOVE MY INDIA "
+echo "  I LOVE MY INDIA "
+echo "  I LOVE MY INDIA "
+echo "  I LOVE MY INDIA "
+echo "  I LOVE MY INDIA "
+echo "  I LOVE MY INDIA "
+
+echo ""
+
+echo  "#####################################"
+echo
+sleep 5
+
+clear
+
+echo  "#####################################"
+echo ""
+echo " follow me instagram "
+echo ""
+echo   " https://www.instagram.com/p/CdrW7cGPj8Q/?igshid=YmMyMTA2M2Y=  "
+echo " "
+echo  "#####################################"
+sleep 3
+ xdg-open https://www.instagram.com/p/CdrW7cGPj8Q/?igshid=YmMyMTA2M2Y=
+
+echo
+
+
+echo " support me , share this tool for your friemds "
+
+sleep 8
+
+clear
+
+clear
+
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+echo " stating package installation ... "
+echo " wait 5 sec  "
+sleep 5
+
+pkg update -y                     
+pkg upgrade -y
+pkg install figlet -y
+pkg install toilet -y
+clear
+pkg install sl
+pkg install libcaca
+pkg install  cmatrix
+clear
+pkg install w3m
+pkg install cowsay
+
+clear
+pkg install telnet
+pkg install Install oneko
+clear 
+pkg Install while
+pkg install mc
+clear
+
+pkg install fortune
+pkg Install fish -y
+clear
+pkg install python -y          
+pkg install python2 -y
+pkg install python3 -y
+clear
+pkg install ruby -y 
+clear
+pkg install git -y
+clear
+
+
+pkg install php -y 
+clear
+pkg install perl -y
+clear
+pkg install nmap -y
+clear
+
+
+pkg install bash -y
+pkg install clang -y
+clear
+pkg install jq -y
+pkg install macchanger -y
+clear
+
+
+pkg install nano -y
+pkg install curl -y
+
+clear
+
+pkg install tar -y
+pkg install zip -y
+
+clear
+
+pkg install unzip -y
+pkg install tor -y
+pkg install wget -y
+
+clear
+
+pkg install wcalc -y 
+pkg install openssl
+pkg install bmon -y
+
+clear
+
+pkg install pip -y
+
+pip install -- upgrade pip
+
+pip install lolcat
+
+lolcat -h
+
+sleep 1
+
+clear
+
+pkg install python3-pip
+
+pkg install wireless-tools
+
+pkg install root-repo
+
+pkg install top
+
+pkg install htop
+
+
+echo " 46 packages  install successfully "
+figlet BYEE  |lolcat -S 143
+
+
+
+
+exit 
+exit
+exit
+
+
+
+
+
+
+
